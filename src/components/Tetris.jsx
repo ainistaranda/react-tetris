d818719c0705aaf32a228a8259adf34878801a37
Ainis Taranda <taranda.ainis@gmail.com>
2023-01-01 13:58:49 -0500

    custom hook that tracks stats of game and displays on game stage

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -7,13 +7,16 @@ import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 import { useInterval } from "../hooks/useInterval";
 import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
+import { useGameStatus } from "../hooks/useGameStatus";
 
 const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage] = useStage(player, resetPlayer);
+  const [stage, setStage, rowsCleared] = useStage(player, resetPlayer);
+  const [score, setScore, rows, setRows, level, setLevel] =
+    useGameStatus(rowsCleared);
 
   console.log("re-render");
 
@@ -29,9 +32,18 @@ const Tetris = () => {
     setDropTime(1000);
     resetPlayer();
     setGameOver(false);
+    setScore(0)
+    setRows(0)
+    setLevel(0)
   };
 
   const drop = () => {
+    // increase level when player has cleared 10 rows
+    if (rows > (level + 1) * 10) {
+      setLevel(prev => prev + 1)
+      // increase speed
+      setDropTime(1000 / (level + 1) + 200)
+    }
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false });
     } else {
@@ -48,14 +60,14 @@ const Tetris = () => {
   const keyUp = ({ keyCode }) => {
     if (!gameOver) {
       if (keyCode === 40) {
-        console.log('Interval On')
-        setDropTime(1000);
+        console.log("Interval On");
+        setDropTime(1000 / (level + 1) + 200);
       }
     }
   };
 
   const dropPlayer = () => {
-    console.log('Interval Off')
+    console.log("Interval Off");
     setDropTime(null);
     drop();
   };
@@ -96,9 +108,9 @@ const Tetris = () => {
             <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
-              <Display text="score" />
-              <Display text="rows" />
-              <Display text="level" />
+              <Display text={`Score: ${score}`} />
+              <Display text={`Rows: ${rows}`} />
+              <Display text={`Level: ${level}`} />
             </div>
           )}
           <StartButton callback={startGame} />
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -1,5 +1,5 @@
 export const TETROMINOS = {
-  0: { shape: [[0]], color: "0,0,0" },
+  0: { shape: [[0]], color: '0,0,0' },
   I: {
     shape: [
               [0, 'I', 0, 0],
@@ -68,3 +68,6 @@ export const randomTetromino = () => {
     tetrominos[Math.floor(Math.random() * tetrominos.length)]
   return TETROMINOS[randTetromino]
 }
+
+
+
